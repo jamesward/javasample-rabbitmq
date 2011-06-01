1fdfc2597c1bca861374f6e3f8232fd8a7264d6d
sclasen <sclasen@salesforce.com>
2011-06-01 12:35:59 -0700

    init stuff

--- src/main/java/com/wabbit/silly/Sender.java
+++ src/main/java/com/wabbit/silly/Sender.java
@@ -6,12 +6,18 @@ import org.springframework.amqp.core.MessageProperties;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 
 public class Sender {
 
     public static void main(String[] args) throws UnsupportedEncodingException, InterruptedException {
+        for (Map.Entry<String, String> entry : System.getenv().entrySet()) {
+            System.out.println(entry.getKey()+"==>"+entry.getValue());
+        }
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:context.xml");
+
+
         AmqpTemplate template = ctx.getBean(AmqpTemplate.class);
         while(true){
             String msg = "Sent at:" + System.currentTimeMillis();
