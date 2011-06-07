02dbef18f08c507425774f39c2a9098d26bb170b
sclasen <sclasen@salesforce.com>
2011-06-07 12:34:30 -0700

    much less verbose form of config

--- src/main/java/com/wabbit/silly/SpringSender.java
+++ src/main/java/com/wabbit/silly/SpringSender.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.UnsupportedEncodingException;
+import java.util.regex.Matcher;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,11 +20,12 @@ public class SpringSender {
 
     public static void main(String[] args) throws UnsupportedEncodingException, InterruptedException {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:context.xml");
-        System.out.println(ctx.getBean("rabbitMqHost"));
-        System.out.println(ctx.getBean("rabbitMqPort"));
-        System.out.println(ctx.getBean("rabbitMqUser"));
-        System.out.println(ctx.getBean("rabbitMqPassword"));
-        System.out.println(ctx.getBean("rabbitMqVHost"));
+        Matcher rabbitMqUrlMatcher = ctx.getBean("rabbitMqUrlMatcher", Matcher.class);
+        System.out.println("user:"+rabbitMqUrlMatcher.group(1));
+        System.out.println("password:"+rabbitMqUrlMatcher.group(2));
+        System.out.println("host:"+rabbitMqUrlMatcher.group(3));
+        System.out.println("port:"+rabbitMqUrlMatcher.group(4));
+        System.out.println("vhost:"+rabbitMqUrlMatcher.group(5));
         RabbitTemplate rabbitTemplate = ctx.getBean(RabbitTemplate.class);
         while(true){
             String msg = "Spring Sent at:" + System.currentTimeMillis();
