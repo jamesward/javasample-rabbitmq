ded5d0a93e59c2aa9add6ccc5a902cfdfc38edb4
sclasen <sclasen@salesforce.com>
2011-06-07 12:00:00 -0700

    example of a spring config that can parse heroku URLs

--- src/test/java/URLRegexTest.java
+++ src/test/java/URLRegexTest.java
@@ -9,7 +9,6 @@ public class URLRegexTest {
 
      @Test
      public void testRegex(){
-         //Matcher matcher = Pattern.compile("amqp://(.+):(.+)@(.+):(.+)(/.+)").matcher("amqp://guest:guest@localhost:5672/");
          Matcher matcher = Pattern.compile("amqp://(.*):(.*)@(.*):(.*)(/.*)").matcher("amqp://guest:guest@localhost:5672/");
 
          matcher.find();
@@ -20,6 +19,14 @@ public class URLRegexTest {
          Assert.assertEquals("localhost", matcher.group(3));
          Assert.assertEquals("5672", matcher.group(4));
          Assert.assertEquals("/", matcher.group(5));
+
+         matcher = Pattern.compile("amqp://(.*):(.*)@(.*):(.*)(/.*)").matcher("amqp://lzuuornl:GaLybFV0m6nuIRBC@iauladza.heroku.srs.rabbitmq.com:16919/iauladza");
+         matcher.find();
+         Assert.assertEquals("lzuuornl", matcher.group(1));
+         Assert.assertEquals("GaLybFV0m6nuIRBC", matcher.group(2));
+         Assert.assertEquals("iauladza.heroku.srs.rabbitmq.com", matcher.group(3));
+         Assert.assertEquals("16919", matcher.group(4));
+         Assert.assertEquals("/iauladza", matcher.group(5));
 
      }
 
